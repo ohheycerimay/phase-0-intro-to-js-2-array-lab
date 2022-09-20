0a6b1eb8a5a7ce121205e427fc3bbb64abf72d22
Ceri Dorsey-Tyler <cerimay@gmail.com>
2022-09-20 11:03:16 -0400

    commit tree 638693d3a32625711d6ebd1b1f6abe84ee3ebfe6

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name) {
+
+    cats.pop(name);
+}
+
+function destructivelyRemoveFirstCat(name) {
+
+    cats.shift(name);
+}
+
+function appendCat(name) {
+
+    //1) appends a cat to the cats array and returns a new array, leaving the cats array unchanged
+    
+    const copyofCats = cats.slice()
+    copyofCats.push(name);
+      return copyofCats
+    }
+
+    function prependCat(name) {
+        
+        const copyofCats = cats.slice()
+        copyofCats.unshift(name);
+          return copyofCats
+        }
+
+function removeLastCat(name) {
+    const copyofCats = cats.slice()
+        copyofCats.pop(name);
+          return copyofCats
+        }
+
+    function removeFirstCat(name) {
+            const copyofCats = cats.slice()
+                copyofCats.shift(name);
+                  return copyofCats
+                }
